25a2ea6d3964e99b2ce1c0c9d1bbb5f391a94611
Yonghwi Kim <yh0921k@gmail.com>
2023-09-17 17:00:00 +0900

    refactor: 한 번의 공연에 대한 요금을 계산하는 switch 함수 추출

--- src/main/java/book/chapter01/Chapter01Application.java
+++ src/main/java/book/chapter01/Chapter01Application.java
@@ -39,25 +39,8 @@ public class Chapter01Application {
               .findFirst()
               .get();
 
-      int thisAmount = 0;
+      int thisAmount = amountFor(perf, play);
 
-      switch (play.getType()) {
-        case "tragedy":
-          thisAmount = 40000;
-          if (perf.getAudience() > 30) {
-            thisAmount += 1000 * (perf.getAudience() - 30);
-          }
-          break;
-        case "comedy":
-          thisAmount = 30000;
-          if (perf.getAudience() > 20) {
-            thisAmount += 10000 + 500 * (perf.getAudience() - 20);
-          }
-          thisAmount += 300 * perf.getAudience();
-          break;
-        default:
-          throw new Exception(String.format("알 수 없는 장르: %s", play.getType()));
-      }
       // 포인트 적립
       volumeCredits += Math.max(perf.getAudience() - 30, 0);
 
@@ -77,6 +60,30 @@ public class Chapter01Application {
     result += String.format("총액: %s\n", format.format(totalAmount / 100));
     result += String.format("적립 포인트: %s점\n", volumeCredits);
     return result;
+  }
+
+  public static int amountFor(Performance perf, Play play) throws Exception {
+    int thisAmount = 0;
+
+    switch (play.getType()) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (perf.getAudience() > 30) {
+          thisAmount += 1000 * (perf.getAudience() - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (perf.getAudience() > 20) {
+          thisAmount += 10000 + 500 * (perf.getAudience() - 20);
+        }
+        thisAmount += 300 * perf.getAudience();
+        break;
+      default:
+        throw new Exception(String.format("알 수 없는 장르: %s", play.getType()));
+    }
+
+    return thisAmount;
   }
 
   public static void main(String[] args) throws Exception {
