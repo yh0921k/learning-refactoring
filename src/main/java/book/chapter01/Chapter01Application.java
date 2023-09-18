81387a95231bf4ea63b176f5734e71b9d0710502
Yonghwi Kim <yh0921k@gmail.com>
2023-09-18 17:00:00 +0900

    refactor: 명시적인 이름 사용하기(2)

--- src/main/java/book/chapter01/Chapter01Application.java
+++ src/main/java/book/chapter01/Chapter01Application.java
@@ -62,22 +62,22 @@ public class Chapter01Application {
     return result;
   }
 
-  public static int amountFor(Performance perf, Play play) throws Exception {
+  public static int amountFor(Performance aPerformance, Play play) throws Exception {
     int result = 0;
 
     switch (play.getType()) {
       case "tragedy":
         result = 40000;
-        if (perf.getAudience() > 30) {
-          result += 1000 * (perf.getAudience() - 30);
+        if (aPerformance.getAudience() > 30) {
+          result += 1000 * (aPerformance.getAudience() - 30);
         }
         break;
       case "comedy":
         result = 30000;
-        if (perf.getAudience() > 20) {
-          result += 10000 + 500 * (perf.getAudience() - 20);
+        if (aPerformance.getAudience() > 20) {
+          result += 10000 + 500 * (aPerformance.getAudience() - 20);
         }
-        result += 300 * perf.getAudience();
+        result += 300 * aPerformance.getAudience();
         break;
       default:
         throw new Exception(String.format("알 수 없는 장르: %s", play.getType()));
