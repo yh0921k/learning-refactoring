c16344699e3d02aaaf21f5d092c9aec9bec6dbb7
Yonghwi Kim <yh0921k@gmail.com>
2023-09-18 17:00:00 +0900

    refactor: play 변수 제거 - 임시 변수를 질의 함수로 바꾸기

--- src/main/java/book/chapter01/Chapter01Application.java
+++ src/main/java/book/chapter01/Chapter01Application.java
@@ -33,11 +33,7 @@ public class Chapter01Application {
     format.setMinimumFractionDigits(2);
 
     for (Performance perf : invoice.getPerformances()) {
-      Play play =
-          Arrays.stream(plays)
-              .filter(p -> p.getPlayId().equals(perf.getPlayId()))
-              .findFirst()
-              .get();
+      Play play = playFor(plays, perf);
 
       int thisAmount = amountFor(perf, play);
 
@@ -60,6 +56,13 @@ public class Chapter01Application {
     result += String.format("총액: %s\n", format.format(totalAmount / 100));
     result += String.format("적립 포인트: %s점\n", volumeCredits);
     return result;
+  }
+
+  private static Play playFor(Play[] plays, Performance perf) {
+    return Arrays.stream(plays)
+        .filter(p -> p.getPlayId().equals(perf.getPlayId()))
+        .findFirst()
+        .get();
   }
 
   public static int amountFor(Performance aPerformance, Play play) throws Exception {
