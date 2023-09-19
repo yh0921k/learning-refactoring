390b8d541a42e4c3a6e191bf91bd4b32e7b899a9
Yonghwi Kim <yh0921k@gmail.com>
2023-09-19 17:00:00 +0900

    refactor: 명세서 클래스 추출하기

--- src/main/java/book/chapter01/Chapter01Application.java
+++ src/main/java/book/chapter01/Chapter01Application.java
@@ -1,15 +1,11 @@
 package book.chapter01;
 
+import book.chapter01.domain.Statement;
 import book.chapter01.dto.Invoice;
-import book.chapter01.dto.Invoice.Performance;
 import book.chapter01.dto.Play;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
-import java.text.NumberFormat;
-import java.util.Arrays;
-import java.util.Currency;
-import java.util.Locale;
 
 public class Chapter01Application {
   public static <T> T readJson(String filepath, Class<T> valueType) {
@@ -23,75 +19,13 @@ public class Chapter01Application {
     }
   }
 
-  public static String statement(Invoice invoice, Play[] plays) throws Exception {
-    int totalAmount = 0;
-    int volumeCredits = 0;
-    String result = String.format("청구 내역 (고객명: %s)\n", invoice.getCustomer());
-
-    NumberFormat format = NumberFormat.getCurrencyInstance(new Locale("en-US"));
-    format.setCurrency(Currency.getInstance("USD"));
-    format.setMinimumFractionDigits(2);
-
-    for (Performance perf : invoice.getPerformances()) {
-      int thisAmount = amountFor(perf, playFor(plays, perf));
-
-      // 포인트 적립
-      volumeCredits += Math.max(perf.getAudience() - 30, 0);
-
-      // 희극 관객 5명마다 추가 포인트 제공
-      if (playFor(plays, perf).getType().equals("comedy")) {
-        volumeCredits += Math.floor(perf.getAudience() / 5);
-      }
-
-      // 청구 내역 출력
-      result +=
-          String.format(
-              "%15s:%12s%4s석\n",
-              playFor(plays, perf).getName(), format.format(thisAmount / 100), perf.getAudience());
-      totalAmount += thisAmount;
-    }
-
-    result += String.format("총액: %s\n", format.format(totalAmount / 100));
-    result += String.format("적립 포인트: %s점\n", volumeCredits);
-    return result;
-  }
-
-  private static Play playFor(Play[] plays, Performance perf) {
-    return Arrays.stream(plays)
-        .filter(p -> p.getPlayId().equals(perf.getPlayId()))
-        .findFirst()
-        .get();
-  }
-
-  public static int amountFor(Performance aPerformance, Play play) throws Exception {
-    int result = 0;
-
-    switch (play.getType()) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.getAudience() > 30) {
-          result += 1000 * (aPerformance.getAudience() - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.getAudience() > 20) {
-          result += 10000 + 500 * (aPerformance.getAudience() - 20);
-        }
-        result += 300 * aPerformance.getAudience();
-        break;
-      default:
-        throw new Exception(String.format("알 수 없는 장르: %s", play.getType()));
-    }
-
-    return result;
-  }
-
   public static void main(String[] args) throws Exception {
     Play[] plays = readJson("resource/plays.json", Play[].class);
     Invoice[] invoices = readJson("resource/invoices.json", Invoice[].class);
 
-    String statement = statement(invoices[0], plays);
-    System.out.println(statement);
+    Statement statement = new Statement();
+    String plainText = statement.readPlainText(invoices[0], plays);
+
+    System.out.println(plainText);
   }
 }
---
+++ src/main/java/book/chapter01/domain/Statement.java
@@ -0,0 +1,75 @@
+package book.chapter01.domain;
+
+import book.chapter01.dto.Invoice;
+import book.chapter01.dto.Play;
+
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Currency;
+import java.util.Locale;
+
+public class Statement {
+  public String readPlainText(Invoice invoice, Play[] plays) throws Exception {
+    int totalAmount = 0;
+    int volumeCredits = 0;
+    String result = String.format("청구 내역 (고객명: %s)\n", invoice.getCustomer());
+
+    NumberFormat format = NumberFormat.getCurrencyInstance(new Locale("en-US"));
+    format.setCurrency(Currency.getInstance("USD"));
+    format.setMinimumFractionDigits(2);
+
+    for (Invoice.Performance perf : invoice.getPerformances()) {
+      int thisAmount = amountFor(perf, playFor(plays, perf));
+
+      // 포인트 적립
+      volumeCredits += Math.max(perf.getAudience() - 30, 0);
+
+      // 희극 관객 5명마다 추가 포인트 제공
+      if (playFor(plays, perf).getType().equals("comedy")) {
+        volumeCredits += Math.floor(perf.getAudience() / 5);
+      }
+
+      // 청구 내역 출력
+      result +=
+          String.format(
+              "%15s:%12s%4s석\n",
+              playFor(plays, perf).getName(), format.format(thisAmount / 100), perf.getAudience());
+      totalAmount += thisAmount;
+    }
+
+    result += String.format("총액: %s\n", format.format(totalAmount / 100));
+    result += String.format("적립 포인트: %s점\n", volumeCredits);
+    return result;
+  }
+
+  private Play playFor(Play[] plays, Invoice.Performance perf) {
+    return Arrays.stream(plays)
+        .filter(p -> p.getPlayId().equals(perf.getPlayId()))
+        .findFirst()
+        .get();
+  }
+
+  public int amountFor(Invoice.Performance aPerformance, Play play) throws Exception {
+    int result = 0;
+
+    switch (play.getType()) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.getAudience() > 30) {
+          result += 1000 * (aPerformance.getAudience() - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.getAudience() > 20) {
+          result += 10000 + 500 * (aPerformance.getAudience() - 20);
+        }
+        result += 300 * aPerformance.getAudience();
+        break;
+      default:
+        throw new Exception(String.format("알 수 없는 장르: %s", play.getType()));
+    }
+
+    return result;
+  }
+}
