4eb51ba19e3a48217daa85610d0de5e35f5c0f1e
Yonghwi Kim <yh0921k@gmail.com>
2023-09-25 17:00:00 +0900

    refactor: volumeCredits 변수를 인라인으로 변경

--- src/main/java/book/chapter01/domain/Statement.java
+++ src/main/java/book/chapter01/domain/Statement.java
@@ -28,10 +28,8 @@ public class Statement {
       totalAmount += amountFor(perf);
     }
 
-    int volumeCredits = totalVolumeCredits();
-
     result += String.format("총액: %s\n", usd(totalAmount));
-    result += String.format("적립 포인트: %s점\n", volumeCredits);
+    result += String.format("적립 포인트: %s점\n", totalVolumeCredits());
     return result;
   }
 
