a4e7046bcf6514c3ea44e6e7ff42313d31c77cbe
Yonghwi Kim <yh0921k@gmail.com>
2023-10-03 19:39:11 +0900

    test: 총수익 계산 검증 테스트 구현 및 공통 로직 추출

--- src/test/java/book/chapter02/ProvinceTest.java
+++ src/test/java/book/chapter02/ProvinceTest.java
@@ -1,5 +1,6 @@
 package book.chapter02;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,16 +8,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ProvinceTest {
 
+  Province province;
+
+  @BeforeEach
+  void beforeEach() {
+    province = SampleProvinceFactory.createSampleProvinceData();
+  }
+
   @Test
   @DisplayName("샘플데이터 생산 부족분 계산 검증")
   void shortFall() {
-    // given
-    Province province = SampleProvinceFactory.createSampleProvinceData();
-
     // when
     int result = province.shortFall();
 
     // then
     assertThat(result).isEqualTo(5);
+  }
+
+  @Test
+  @DisplayName("샘플데이터 총수익 계산 검증")
+  void profit() {
+    // when
+    int result = province.profit();
+
+    // then
+    assertThat(result).isEqualTo(230);
   }
 }
