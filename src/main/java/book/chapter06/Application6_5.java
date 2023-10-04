7860828c893947f540815cc5641ddac90ca4a67b
Yonghwi Kim <yh0921k@gmail.com>
2023-10-04 17:00:00 +0900

    refactor: 함수 추출하기(6.1절)

--- src/main/java/book/chapter06/Application6_5.java
+++ src/main/java/book/chapter06/Application6_5.java
@@ -27,6 +27,10 @@ public class Application6_5 {
   public static boolean isNewEngland(Customer customer) {
     String stateCode = customer.getAddress().getState();
 
+    return refactoredIsNewEngland(stateCode);
+  }
+
+  public static boolean refactoredIsNewEngland(String stateCode) {
     return Arrays.stream(new String[] {"MA", "CT", "ME", "VT", "NH", "RI"})
         .anyMatch(code -> code.equals(stateCode));
   }
