0401b659147693d9d71f2f458e5c59581ef7f494
Yonghwi Kim <yh0921k@gmail.com>
2023-10-04 17:00:00 +0900

    feat: 함수 선언 변경하기(6.5절) 기본 코드

---
+++ src/main/java/book/chapter06/Application6_5.java
@@ -0,0 +1,43 @@
+package book.chapter06;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class Application6_5 {
+  public static void main(String[] args) {
+    List<Customer> someCustomers = new LinkedList<>();
+    someCustomers.add(new Customer(new Address("MA")));
+    someCustomers.add(new Customer(new Address("RI")));
+    someCustomers.add(new Customer(new Address("ME")));
+    someCustomers.add(new Customer(new Address("MA")));
+    someCustomers.add(new Customer(new Address("CT")));
+    someCustomers.add(new Customer(new Address("NO")));
+    someCustomers.add(new Customer(new Address("NO")));
+    someCustomers.add(new Customer(new Address("NO")));
+    someCustomers.add(new Customer(new Address("NO")));
+    someCustomers.add(new Customer(new Address("NO")));
+
+    long numberOfNewEngland = someCustomers.stream().filter(Application6_5::isNewEngland).count();
+    System.out.println("numberOfNewEngland = " + numberOfNewEngland);
+  }
+
+  public static boolean isNewEngland(Customer customer) {
+    return Arrays.stream(new String[] {"MA", "CT", "ME", "VT", "NH", "RI"})
+        .anyMatch(code -> code.equals(customer.getAddress().getState()));
+  }
+}
+
+@Data
+@AllArgsConstructor
+class Customer {
+  private Address address;
+}
+
+@Data
+@AllArgsConstructor
+class Address {
+  private String state;
+}
