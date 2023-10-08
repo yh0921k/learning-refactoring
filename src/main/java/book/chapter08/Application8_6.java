223a698ef226e3fd8a4b9ef5b016842991b16a43
Yonghwi Kim <yh0921k@gmail.com>
2023-10-08 17:00:00 +0900

    feat: 문장 슬라이드하기(8.6절) 기본 코드2

--- src/main/java/book/chapter08/Application8_6.java
+++ src/main/java/book/chapter08/Application8_6.java
@@ -1,45 +1,20 @@
 package book.chapter08;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Application8_6 {
   public static void main(String[] args) {
-    PricingPlan pricingPlan = retrievePricingPlan();
-    Order order = retrieveOrder();
-    int baseCharge = pricingPlan.base;
-    int charge;
-    int chargePerUnit = pricingPlan.unit;
-    int units = order.units;
-    int discount;
-    charge = baseCharge + units * chargePerUnit;
-    int discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0);
-    discount = (int) (discountableUnits * pricingPlan.discountFactor);
-    if (order.isRepeat) discount += 20;
-    charge = charge - discount;
-    chargeOrder(charge);
+    Queue<String> availableResources = new LinkedList<>();
+    Queue<String> allocatedResources = new LinkedList<>();
+    String result;
+
+    if(availableResources.size() == 0) {
+      result = "create result";
+      allocatedResources.add(result);
+    } else {
+      result = availableResources.remove();
+      allocatedResources.add(result);
+    }
   }
-
-  private static void chargeOrder(int charge) {
-    System.out.println("Application8_6.chargeOrder");
-  }
-
-  private static Order retrieveOrder() {
-    System.out.println("Application8_6.retrieveOrder");
-    return new Order();
-  }
-
-  private static PricingPlan retrievePricingPlan() {
-    System.out.println("Application8_6.retrievePricingPlan");
-    return new PricingPlan();
-  }
-}
-
-class Order {
-  int units;
-  boolean isRepeat;
-}
-
-class PricingPlan {
-  int unit;
-  double discountFactor;
-  int base;
-  int discountThreshold;
 }
