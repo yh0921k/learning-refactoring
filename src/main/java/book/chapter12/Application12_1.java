98ebcdc4dc7dfaa4808312d5d62180111584b748
Yonghwi Kim <yh0921k@gmail.com>
2023-10-10 17:00:00 +0900

    feat: 메서드 올리기(12.1절) 기본 코드

---
+++ src/main/java/book/chapter12/Application12_1.java
@@ -0,0 +1,23 @@
+package book.chapter12;
+
+public class Application12_1 {
+  public static void main(String[] args) {}
+}
+
+class Party {}
+
+class Employee extends Party {
+  int monthlyCost;
+
+  public int annualCost() {
+    return monthlyCost * 12;
+  }
+}
+
+class Department extends Party {
+  int monthlyCost;
+
+  public int totalAnnualCost() {
+    return monthlyCost * 12;
+  }
+}
