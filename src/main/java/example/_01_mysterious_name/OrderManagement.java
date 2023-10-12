a7697ae65b0b8d6948da41d920cc030898808c69
Yonghwi Kim <yh0921k@gmail.com>
2023-10-12 17:00:00 +0900

    refactor: 람다식 변경

--- src/main/java/example/_01_mysterious_name/OrderManagement.java
+++ src/main/java/example/_01_mysterious_name/OrderManagement.java
@@ -35,8 +35,8 @@ public class OrderManagement {
   public static void main(String[] args) {
     OrderManagement orderManagement = new OrderManagement();
     orderManagement.loadOrders();
-    orderManagement.getUsernames().forEach(name -> System.out.println(name));
-    orderManagement.getOrderNumbers().forEach(orderNumber -> System.out.println(orderNumber));
+    orderManagement.getUsernames().forEach(n -> System.out.println(n));
+    orderManagement.getOrderNumbers().forEach(on -> System.out.println(on));
   }
 
   private static List<UserOrder> getUserOrderFromAPI() {
