17345874ef3cca6ec3417f46d5dfaa0a67a82a90
Yonghwi Kim <yh0921k@gmail.com>
2023-10-13 17:00:00 +0900

    feat: 기이한 이름(Mysterious Name) - 변수 이름 바꾸기

--- src/main/java/example/_01_mysterious_name/OrderManagement.java
+++ src/main/java/example/_01_mysterious_name/OrderManagement.java
@@ -1,9 +1,7 @@
 package example._01_mysterious_name;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,10 +15,12 @@ public class OrderManagement {
    * 주어진 사용자 주문 리스트에 대해 각 사용자 이름과 주문번호를 읽어온다.
    */
   private void loadOrders() {
-    List<UserOrder> userOrders = getUserOrderFromAPI();
-    for (UserOrder userOrder : userOrders) {
-      usernames.add(userOrder.getUsername());
-      orderNumbers.add(userOrder.getOrderNumber());
+    UserInfos data = getUserInfosFromAPI();
+
+    List<UserInfo> userInfos = data.getUserInfos();
+    for (UserInfo userInfo : userInfos) {
+      usernames.add(userInfo.getUsername());
+      orderNumbers.add(userInfo.getOrderNumber());
     }
   }
 
@@ -39,19 +39,30 @@ public class OrderManagement {
     orderManagement.getOrderNumbers().forEach(System.out::println);
   }
 
-  private static List<UserOrder> getUserOrderFromAPI() {
-    ArrayList<UserOrder> userOrders = new ArrayList<>();
-    userOrders.add(new UserOrder("A", "A001"));
-    userOrders.add(new UserOrder("B", "B001"));
-    userOrders.add(new UserOrder("C", "C001"));
-    return userOrders;
+  private static UserInfos getUserInfosFromAPI() {
+    UserInfos userInfos = new UserInfos();
+    userInfos.add(new UserInfo("A", "A001"));
+    userInfos.add(new UserInfo("B", "B001"));
+    userInfos.add(new UserInfo("C", "C001"));
+    return userInfos;
   }
 }
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-class UserOrder {
+class UserInfos {
+  private List<UserInfo> userInfos = new LinkedList<>();
+
+  public void add(UserInfo userInfo) {
+    this.userInfos.add(userInfo);
+  }
+}
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+class UserInfo {
   private String username;
   private String orderNumber;
 }
