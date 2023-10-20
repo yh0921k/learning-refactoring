2c799a786b9de230249c40140c65ccb21eccc892
Yonghwi Kim <yh0921k@gmail.com>
2023-10-20 17:00:00 +0900

    feat: 반복문 쪼개기(Split Loop) 코드베이스 추가

---
+++ src/main/java/book/chapter08/Application8_7.java
@@ -0,0 +1,36 @@
+package book.chapter08;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+public class Application8_7 {
+  public static void main(String[] args) {
+    List<Person> persons = getPersonList();
+
+    int youngest = persons.isEmpty() ? Integer.MAX_VALUE : persons.get(0).getAge();
+    int totalSalary = 0;
+    for (Person person : persons) {
+      if (person.getAge() < youngest) {
+        youngest = person.getAge();
+      }
+      totalSalary += person.getSalary();
+    }
+
+    System.out.println("youngest = " + youngest);
+    System.out.println("totalSalary = " + totalSalary);
+  }
+
+  private static List<Person> getPersonList() {
+    return List.of(new Person(20, 5000), new Person(30, 8000), new Person(50, 4000));
+  }
+}
+
+@Getter
+@AllArgsConstructor
+@ToString
+class Person {
+  private int age;
+  private int salary;
+}
