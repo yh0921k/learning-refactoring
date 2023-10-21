e770a6844764971b351380bf0805ff93bac5288e
Yonghwi Kim <yh0921k@gmail.com>
2023-10-21 17:00:00 +0900

    refactor: 각 반복문을 함수로 추출

--- src/main/java/book/chapter08/Application8_7.java
+++ src/main/java/book/chapter08/Application8_7.java
@@ -9,20 +9,29 @@ public class Application8_7 {
   public static void main(String[] args) {
     List<Person> persons = getPersonList();
 
+    int youngest = getYoungest(persons);
+    int totalSalary = getTotalSalary(persons);
+
+    System.out.println("youngest = " + youngest);
+    System.out.println("totalSalary = " + totalSalary);
+  }
+
+  private static int getTotalSalary(List<Person> persons) {
+    int totalSalary = 0;
+    for (Person person : persons) {
+      totalSalary += person.getSalary();
+    }
+    return totalSalary;
+  }
+
+  private static int getYoungest(List<Person> persons) {
     int youngest = persons.isEmpty() ? Integer.MAX_VALUE : persons.get(0).getAge();
     for (Person person : persons) {
       if (person.getAge() < youngest) {
         youngest = person.getAge();
       }
     }
-
-    int totalSalary = 0;
-    for (Person person : persons) {
-      totalSalary += person.getSalary();
-    }
-
-    System.out.println("youngest = " + youngest);
-    System.out.println("totalSalary = " + totalSalary);
+    return youngest;
   }
 
   private static List<Person> getPersonList() {
