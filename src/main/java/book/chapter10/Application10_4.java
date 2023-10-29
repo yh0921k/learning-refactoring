bf4f465d7b2eeb95006fd8ace335d8834a5ea6bc
Yonghwi Kim <yh0921k@gmail.com>
2023-10-29 13:00:00 +0900

    refactor: 팩토리 메서드 및 서브 클래스 구현

--- src/main/java/book/chapter10/Application10_4.java
+++ src/main/java/book/chapter10/Application10_4.java
@@ -3,8 +3,7 @@ package book.chapter10;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 public class Application10_4 {
   public static void main(String[] args) {
@@ -14,27 +13,51 @@ public class Application10_4 {
     System.out.println("speeds(birds) = " + speeds(birds));
   }
 
+  public static Bird createBird(Bird bird) {
+    switch (bird.type) {
+      case "유럽 제비":
+        return new EuropeanSwallow(bird);
+      case "아프리카 제비":
+        return new AfricanSwallow(bird);
+      case "노르웨이 파랑 앵무":
+        return new NorwegianBlueParrot(bird);
+      default:
+        return new Bird(bird);
+    }
+  }
+
   private static List<Bird> createSampleBirds() {
     return List.of(new Bird("유럽 제비"), new Bird("아프리카 제비"), new Bird("노르웨이 파랑 앵무"), new Bird("없는종"));
   }
 
   public static Map<String, String> plumages(List<Bird> birds) {
-    return birds.stream().collect(Collectors.toMap(bird -> bird.type, Bird::plumage));
+    return birds.stream()
+        .map(bird -> createBird(bird))
+        .collect(Collectors.toMap(bird -> bird.type, Bird::plumage));
   }
 
   public static Map<String, Integer> speeds(List<Bird> birds) {
     return birds.stream()
+        .map(bird -> createBird(bird))
         .collect(Collectors.toMap(bird -> bird.type, Bird::airSpeedVelocity));
   }
 }
 
 @Data
-@RequiredArgsConstructor
+@NoArgsConstructor
 class Bird {
-  public final String type;
+  public String type;
   public int numberOfCoconuts;
   public int voltage;
   public boolean isNailed;
+
+  public Bird(String type) {
+    this.type = type;
+  }
+
+  public Bird(Bird bird) {
+    this.type = bird.type;
+  }
 
   public String plumage() {
     switch (type) {
@@ -60,5 +83,23 @@ class Bird {
       default:
         return 0;
     }
+  }
+}
+
+class EuropeanSwallow extends Bird {
+  public EuropeanSwallow(Bird bird) {
+    super(bird);
+  }
+}
+
+class AfricanSwallow extends Bird {
+  public AfricanSwallow(Bird bird) {
+    super(bird);
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  public NorwegianBlueParrot(Bird bird) {
+    super(bird);
   }
 }
