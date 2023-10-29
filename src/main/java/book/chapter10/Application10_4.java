9d3c74ea8aa7179cc8b4d2a386438308220e7f4b
Yonghwi Kim <yh0921k@gmail.com>
2023-10-29 13:00:00 +0900

    refactor: 연관된 데이터와 기능을 클래스로 묶기

--- src/main/java/book/chapter10/Application10_4.java
+++ src/main/java/book/chapter10/Application10_4.java
@@ -1,43 +1,64 @@
 package book.chapter10;
 
-import lombok.AllArgsConstructor;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 public class Application10_4 {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    List<Bird> birds = createSampleBirds();
 
-  public static String plumage(Bird bird) {
-    switch (bird.type) {
+    System.out.println("plumages(birds) = " + plumages(birds));
+    System.out.println("speeds(birds) = " + speeds(birds));
+  }
+
+  private static List<Bird> createSampleBirds() {
+    return List.of(new Bird("유럽 제비"), new Bird("아프리카 제비"), new Bird("노르웨이 파랑 앵무"), new Bird("없는종"));
+  }
+
+  public static Map<String, String> plumages(List<Bird> birds) {
+    return birds.stream().collect(Collectors.toMap(bird -> bird.type, Bird::plumage));
+  }
+
+  public static Map<String, Integer> speeds(List<Bird> birds) {
+    return birds.stream()
+        .collect(Collectors.toMap(bird -> bird.type, Bird::airSpeedVelocity));
+  }
+}
+
+@Data
+@RequiredArgsConstructor
+class Bird {
+  public final String type;
+  public int numberOfCoconuts;
+  public int voltage;
+  public boolean isNailed;
+
+  public String plumage() {
+    switch (type) {
       case "유럽 제비":
         return "보통이다.";
       case "아프리카 제비":
-        return bird.numberOfCoconuts > 2 ? "지쳤다." : "보통이다.";
+        return numberOfCoconuts > 2 ? "지쳤다." : "보통이다.";
       case "노르웨이 파랑 앵무":
-        return bird.voltage > 100 ? "그을렸다." : "예쁘다";
+        return voltage > 100 ? "그을렸다." : "예쁘다";
       default:
         return "알 수 없다.";
     }
   }
 
-  public static int airSpeedVelocity(Bird bird) {
-    switch (bird.type) {
+  public int airSpeedVelocity() {
+    switch (type) {
       case "유럽 제비":
         return 35;
       case "아프리카 제비":
-        return 40 - 2 * bird.numberOfCoconuts;
+        return 40 - 2 * numberOfCoconuts;
       case "노르웨이 파랑 앵무":
-        return bird.isNailed ? 0 : 10 + bird.voltage / 10;
+        return isNailed ? 0 : 10 + voltage / 10;
       default:
         return 0;
     }
   }
-}
-
-@Data
-@AllArgsConstructor
-class Bird {
-  String type;
-  int numberOfCoconuts;
-  int voltage;
-  boolean isNailed;
 }
