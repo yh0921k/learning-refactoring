0eec64ad74830edd417632af5264cd0f123e798f
Yonghwi Kim <yh0921k@gmail.com>
2023-11-12 17:00:00 +0900

    refactor: 조건절 변수 추출하기

--- src/main/java/book/chapter11/Application11_4.java
+++ src/main/java/book/chapter11/Application11_4.java
@@ -8,7 +8,8 @@ public class Application11_4 {
     int low = room.daysTempRange.low;
     int high = room.daysTempRange.high;
 
-    if (!heatingPlan.withinRange(low, high)) {
+    boolean isWithinRange = heatingPlan.withinRange(low, high);
+    if (!isWithinRange) {
       throw new Exception("방 온도가 지정 범위를 벗어났습니다.");
     }
   }
