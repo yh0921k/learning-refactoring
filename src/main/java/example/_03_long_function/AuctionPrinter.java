bda2451f919dda4966d76f2d13d1d90fc36be709
Yonghwi Kim <yh0921k@gmail.com>
2023-11-29 19:00:00 +0900

    refactor: Command 구현 - execute() 메서드에 필요한 메서드 옮기기

--- src/main/java/example/_03_long_function/AuctionPrinter.java
+++ src/main/java/example/_03_long_function/AuctionPrinter.java
@@ -19,4 +19,37 @@ public class AuctionPrinter {
       fileWriter.close();
     }
   }
+
+  private String getMarkdownForParticipant(Participant p) {
+    return String.format(
+        "| %s %s | %.2f%% |\n", p.getName(), createMark(p), p.getRate(totalNumberOfAuctions));
+  }
+
+  /* |:white_check_mark:|:white_check_mark:|:white_check_mark:|:x:| */
+  private StringBuilder createMark(Participant p) {
+    StringBuilder line = new StringBuilder();
+    for (int i = 1; i <= totalNumberOfAuctions; i++) {
+      if (p.getParticipatingAuctions().containsKey(i) && p.getParticipatingAuctions().get(i)) {
+        line.append("|:white_check_mark:");
+      } else {
+        line.append("|:x:");
+      }
+    }
+    return line;
+  }
+
+  /* | 참여자 (420) | 1주차 | 2주차 | 3주차 | 참석율 | | --- | --- | --- | --- | --- | */
+  private StringBuilder createHeader(int totalNumberOfParticipants) {
+    StringBuilder header =
+        new StringBuilder(String.format("| 참여자 (%d) |", totalNumberOfParticipants));
+
+    for (int index = 1; index <= totalNumberOfAuctions; index++) {
+      header.append(String.format(" %d주차 |", index));
+    }
+    header.append(" 참석율 |\n");
+
+    header.append("| --- ".repeat(Math.max(0, totalNumberOfAuctions + 2)));
+    header.append("|\n");
+    return header;
+  }
 }
