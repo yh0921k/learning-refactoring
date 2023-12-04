52ffae12becb92dcf2b94a37d34600a2434bac0b
Yonghwi Kim <yh0921k@gmail.com>
2023-12-04 19:00:00 +0900

    feat: 첫 입찰자가 낙찰자인 경우 계산 및 출력 기능 추가 구현

--- src/main/java/example/_03_long_function/AuctionDashboard.java
+++ src/main/java/example/_03_long_function/AuctionDashboard.java
@@ -1,6 +1,7 @@
 package example._03_long_function;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
@@ -24,6 +25,8 @@ public class AuctionDashboard {
 
   public void print() throws IOException, InterruptedException {
     List<Participant> participants = new CopyOnWriteArrayList<>();
+    Participant[] sameBidders = new Participant[totalNumberOfAuctions];
+
     ExecutorService service = Executors.newFixedThreadPool(4);
     CountDownLatch latch = new CountDownLatch(totalNumberOfAuctions);
 
@@ -36,9 +39,32 @@ public class AuctionDashboard {
               try {
                 Auction auction = auctionHub.getAuction(auctionId);
                 List<History> histories = auction.getHistories();
+
+                Long firstBidAmount = Long.MAX_VALUE;
+                Participant firstBidder = null;
+
+                Long lastBidAmount = 0L;
+                Participant lastBidder = null;
+
                 for (History history : histories) {
                   Participant participant = findParticipant(history.getUserName(), participants);
                   participant.participate(auctionId);
+
+                  if (firstBidder == null || history.getBidAmount() < firstBidAmount) {
+                    firstBidder = history.getParticipant();
+                    firstBidAmount = history.getBidAmount();
+                  }
+
+                  if (firstBidder == null || history.getBidAmount() > lastBidAmount) {
+                    lastBidder = history.getParticipant();
+                    lastBidAmount = history.getBidAmount();
+                  }
+                }
+
+                if (firstBidder == lastBidder) {
+                  sameBidders[auctionId - 1] = lastBidder;
+                } else {
+                  sameBidders[auctionId - 1] = null;
                 }
 
                 latch.countDown();
@@ -53,6 +79,9 @@ public class AuctionDashboard {
     service.shutdown();
 
     new AuctionPrinter(participants, totalNumberOfAuctions).execute();
+    Arrays.stream(sameBidders)
+        .filter(bidder -> bidder != null)
+        .forEach((bidder) -> System.out.println("bidder = " + bidder.getName()));
   }
 
   private Participant findParticipant(String username, List<Participant> participants) {
