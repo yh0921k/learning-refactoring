e494e3a735fa3711007e1743bbdbfeb3c4f2c76f
Yonghwi Kim <yh0921k@gmail.com>
2023-12-13 19:00:00 +0900

    refactor: 조건문을 다형성으로 바꾸기 - 공통 메서드 protected 변경 및 추상 클래스화

--- src/main/java/example/_03_long_function/AuctionPrinter.java
+++ src/main/java/example/_03_long_function/AuctionPrinter.java
@@ -1,39 +1,21 @@
 package example._03_long_function;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Comparator;
 import java.util.List;
 
-public class AuctionPrinter {
+public abstract class AuctionPrinter {
   protected final List<Participant> participants;
   protected final int totalNumberOfAuctions;
 
-  public AuctionPrinter(
-      List<Participant> participants, int totalNumberOfAuctions) {
+  public AuctionPrinter(List<Participant> participants, int totalNumberOfAuctions) {
     this.participants = participants;
     this.totalNumberOfAuctions = totalNumberOfAuctions;
   }
 
-  public void execute() throws IOException {
-    switch (printerMode) {
-      case CSV -> {
-
-      }
-      case CONSOLE -> {
-
-      }
-      case MARKDOWN -> {
-
-      }
-    }
-  }
-
-
+  public abstract void execute() throws IOException;
 
   /* |:white_check_mark:|:white_check_mark:|:white_check_mark:|:x:| */
-  private StringBuilder createMark(Participant p) {
+  protected StringBuilder createMark(Participant p) {
     StringBuilder line = new StringBuilder();
     for (int i = 1; i <= totalNumberOfAuctions; i++) {
       if (p.getParticipatingAuctions().containsKey(i) && p.getParticipatingAuctions().get(i)) {
